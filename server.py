39a3c7e8157b67252807d429fdabd35421fb77c6
Lemon-tech24 <78329279+Lemon-tech24@users.noreply.github.com>
2021-04-01 10:25:06 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import socket
 
 s = socket.socket()
-ipaddr = socket.gethostbyname(socket.gethostname())
-s.bind((ipaddr, 8080))
+
+s.bind(('localhost', 8080))
 
 s.listen(3)
 
 print('waiting for connections')
-allowed_conn = 5
+
 while True:
     conn, addr = s.accept()
     name = conn.recv(1024).decode()
